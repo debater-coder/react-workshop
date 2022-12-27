662b04f4a7dc2c5160d651363a39eb69881d67f2
Hamzah Ahmed <hamzah@syedahmed.net>
2022-12-27 21:33:43 +1100

    feat: moar slides

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -1,14 +1,17 @@
-export default function Slide({
-  children,
-  notes,
-}: {
+export interface SlideProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLElement>,
+    HTMLElement
+  > {
   children: React.ReactNode;
   notes?: React.ReactNode | string;
-}) {
+}
+
+export default function Slide(props: SlideProps) {
   return (
-    <section>
-      {children}
-      <aside>{notes}</aside>
+    <section {...props}>
+      {props.children}
+      <aside className="notes">{props.notes}</aside>
     </section>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,49 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import Slide from "./components/Slide";
 import "./index.css";
-import { Sandpack } from "@codesandbox/sandpack-react";
+import {
+  Sandpack,
+  SandpackCodeEditor,
+  SandpackLayout,
+  SandpackProvider,
+} from "@codesandbox/sandpack-react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App>
-      <Slide>
-        <h1>Slide 1</h1>
+      <Slide notes="give a short intro">
+        <h1>
+          <code>Reveal.js</code> test
+        </h1>
         <p>Slide 1 content</p>
       </Slide>
       <Slide>
-        <h1>Slide 2</h1>
+        <h2>Slide 2</h2>
         <p>Let's try to embed a sandbox</p>
-        <Sandpack template="react" theme={"dark"} />
+        <Sandpack
+          template="react"
+          theme={"dark"}
+          options={{
+            showNavigator: true,
+            showLineNumbers: true,
+            showTabs: true,
+          }}
+        />
       </Slide>
       <Slide>
-        <h1>Slide 3</h1>
-        <p>Slide 3 content</p>
+        <h2>Slide 3</h2>
+        <p>Here's some code (no editor):</p>
+        <SandpackProvider template="react" theme={"dark"}>
+          <SandpackLayout>
+            <SandpackCodeEditor />
+          </SandpackLayout>
+        </SandpackProvider>
+      </Slide>
+      <Slide
+        data-background-image="http://placekitten.com/g/1920/1080"
+        data-background-opacity="0.5"
+      >
+        <h2 className="r-fit-text">This is a kitten</h2>
       </Slide>
     </App>
   </React.StrictMode>
